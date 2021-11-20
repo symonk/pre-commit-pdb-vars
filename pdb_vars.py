acee861c0d8981e37ab95b7a055d6b391f08fca5
symonk <jackofspaces@gmail.com>
2021-11-20 14:15:57 +0000

    apply pre-commit hooks to the code; add shortcut tuple

--- pdb_vars.py
+++ pdb_vars.py
@@ -1,6 +1,44 @@
 import argparse
 import typing
 
+shortcuts = (
+    "h",
+    "w",
+    "d",
+    "u",
+    "b",
+    "tbreak",
+    "cl",
+    "disable",
+    "enable",
+    "ignore",
+    "condition",
+    "commands",
+    "s",
+    "n",
+    "unt",
+    "r",
+    "c",
+    "j",
+    "l",
+    "ll",
+    "a",
+    "p",
+    "pp",
+    "whatis",
+    "source",
+    "display",
+    "undisplay",
+    "interact",
+    "alias",
+    "unalias",
+    "run",
+    "restart",
+    "q",
+    "debug",
+    "retval",
+)
+
 
 def main(argv: typing.Optional[typing.Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
@@ -10,7 +48,8 @@ def main(argv: typing.Optional[typing.Sequence[str]] = None) -> int:
 
 
 def check_vars(filenames):
-    print(filenames)
+    for file in filenames:
+        print(file)
     return 1
 
 
