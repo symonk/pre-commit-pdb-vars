e6f3fac9b81d79a1619e960da85af4c4a8279a23
symonk <jackofspaces@gmail.com>
2021-11-21 19:19:22 +0000

    actual ast NodeVisitor impl; tidy up errors; capture files, cols and linenos better

--- pdbvars/main.py
+++ pdbvars/main.py
@@ -55,14 +55,61 @@ def main(argv: typing.Optional[typing.Sequence[str]] = None) -> int:
     return check_vars(args.filenames)
 
 
+class PdbParser(ast.NodeVisitor):
+    """
+    Captures `violations` of pdb command variable naming.  At present we check that
+    standard assignments and function names do not mirror that of a pdb command shortcut.
+
+        :: Supports
+            :: Function Def
+            :: Name
+            :: More coming in future.
+    """
+
+    def __init__(self) -> None:
+        self.violations: typing.List[Violation] = []
+        self.current_file = ""  # Todo: Terrible concept; refactor.
+
+    def visit_Name(self, node: ast.Name) -> typing.Any:
+        if node.id in PDB_SHORTCUTS:
+            self.violations.append(
+                Violation(
+                    self.current_file, "name", node.lineno, node.col_offset, node.id
+                )
+            )
+
+    def visit_FunctionDef(self, node: ast.FunctionDef) -> typing.Any:
+        if node.name in PDB_SHORTCUTS:
+            self.violations.append(
+                Violation(
+                    self.current_file,
+                    "function def",
+                    node.lineno,
+                    node.col_offset,
+                    node.name,
+                )
+            )
+
+
 @dataclasses.dataclass(frozen=True)
 class Violation:
     mod: str
+    node_type: str
     line_no: int
+    column: int
     shortcut: str
 
     def __repr__(self) -> str:
-        return f"Prohibited PDB Command variable `{self.shortcut}` at: {self.mod}:{self.line_no}"
+        return (
+            f"Prohibited PDB Command {self.node_type} "
+            f"`{self.shortcut}` at: "
+            f"{os.path.abspath(self.mod)}:{self.line_no}:{self.column}."
+        )
+
+
+def _walk_nodes(filepath: str, visitor: ast.NodeVisitor):
+    with open(filepath) as f:
+        visitor.visit(ast.parse(f.read(), filename=filepath))
 
 
 def check_vars(filenames: str):
@@ -70,42 +117,19 @@ def check_vars(filenames: str):
     For each of the file paths passed to the hook; parse the AST for the given file
     and find all variable names, then match them against the predefined `shortcuts`
     of pdb commands.  If any variable is in violation, exit 1 else exit 0.
+
     :param filenames: Sequence of files passed to the hook
     :return: The exit code.
     """
-
-    def _walk_nodes(root):
-        return [
-            node
-            for node in sorted(
-                {
-                    node
-                    for node in ast.walk(root)
-                    if isinstance(node, ast.Name) and not isinstance(node.ctx, ast.Load)
-                }
-            )
-        ]
-
+    visitor = PdbParser()
     for file in filenames:
-        with open(file) as f:
-            root = ast.parse(f.read())
-            for node in _walk_nodes(root):
-                for prohibited in PDB_SHORTCUTS:
-                    if node.id == prohibited:
-                        print(Violation(os.path.abspath(file), node.lineno, node.id))
-                        return 1
-
+        visitor.current_file = file
+        _walk_nodes(file, visitor)
+        if visitor.violations:
+            for violation in visitor.violations:
+                print(violation)
+            return 1
     return 0
-
-
-def matches(variable_name: str) -> bool:
-    """
-    Check if the parsed variable name is an offender, if an offender is found
-    exit code `1` will be returned downstream.
-    :param variable_name: The parsed python file variable name
-    :return: Boolean indicating if the variable name is prohibited.
-    """
-    return variable_name in PDB_SHORTCUTS
 
 
 if __name__ == "__main__":
---
+++ test_files/fail.py
@@ -0,0 +1,14 @@
+q = "nice"
+
+s = "one"
+
+enable = "alias"
+
+
+def alias():
+    pass
+
+
+class C:
+    def disable(self):
+        pass
